a4d6bda0ed0be9c0805a8bc98c3443db29a6ee11
Luke Hewitt <lbh@Luke-Black.local>
2025-10-11 21:25:08 -0700

    use only 50% of cores

--- config.R
+++ config.R
@@ -1,4 +1,4 @@
-n_workers = parallel::detectCores() # Parallelize code across all cores (reduce this number if memory consumption is too high)
+n_workers = ceiling(parallel::detectCores()*0.5) # Parallelize code across half of cores (reduce this number if memory consumption is too high)
 use_analysis_cache = T # Set to false to run all analyses from scratch
 n_runs = 32 # Number of repeats of main analysis (sampling different "control groups" and outcomes)
 
